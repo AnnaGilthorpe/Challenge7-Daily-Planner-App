50319a5a920f05fdb8d7ad6a9f63538ace7a27d9
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-01-23 14:11:10 +0000

    refactor code start

--- script.js
+++ script.js
@@ -3,61 +3,59 @@
 // console.log(m.toString());
 // console.log("hello world");
 
-var today = moment();
-let dayWeek = today.format("[Happy] dddd[!]")
-$("#currentDay").text(dayWeek);
-console.log(dayWeek);
+// var today = moment();
+// let dayWeek = today.format("[Happy] dddd[!]")
+// $("#currentDay").text(dayWeek);
+// console.log(dayWeek);
 
-let currentHour = today.format("H")
-console.log(currentHour)
+// let currentHour = today.format("H")
+// console.log(currentHour)
 
-// let nine = document.querySelector("#9")
+// let dataTime = document.querySelector('#data-time');
 
-// //9am
-function nineam () {
-if (currentHour > 9) {
-$("textarea").addClass("past")
-}
-if (currentHour < 9) {
-    $("textarea").addClass("future")
-}
-if (currentHour === 9) {
-    $("textarea").addClass("present")
-}
-localStorage.setItem("9am", "something")
-$("textarea").text(localStorage.getItem("9am"))
-}
 
-$("button").on("click", function(event){
-    $("saveBtn").setClass("fas fa-sync fa-spin")
-    console.log($(event.target).siblings("textarea").val());
-    console.log($(event.target).attr("data-time"));
 
+
+
+$(document).ready(function () {
+    $('.saveBtn').on('click', function () {
+      let value = $(this).siblings('.work-description').val()
+      let time = $(this).parent().attr('id')
+
+      localStorage.setItem(time, JSON.stringify(value))
+    })
+
+    function updateHours() {
+        let currentHour = moment().hour();
+        
+        $('.time-area').each(function () {
+            let currentBlockTime = parseInt($(this).attr('id').split('-')[0])
+            
+            if (currentBlockTime < currentHour) {
+                $(this).addClass('past')
+            } else if (currentBlockTime === currentHour) {
+                $(this).removeClass('past')
+                $(this).addClass('present')
+            } else {
+                $(this).removeClass('past')
+                $(this).removeClass('present')
+                $(this).removeClass('future')
+            }
+        })
+    }
+
+    updateHours()
+
+    setInterval(updateHours, 540000)
+
+
+    // bug, work out why local storage isnt dispalying after refresh
+
+    // TODO: finish this section for every hour, ensure you have id with time
+    // on each parent eg: '11-am'
+    /// and class of work descriotion on each text area
+    $('#9-am .work-description').val(localStorage.getItem('9-am'))
+    $('#10-am .work-description').val(localStorage.getItem('10-am'))
+    // dispaly the current day and time on page 
+    // look up how to do this on moment
 })
-//10am
-
-
-
-//11am
-
-
-
-
-//12pm
-
-
-
-//1pm
-
-
-//2pm
-
-
-
-//3pm
-
-
-//4pm
-
-
-//5pm
