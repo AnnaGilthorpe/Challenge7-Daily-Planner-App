37aaeef2a6b1d31ac319f388c568f921ad20ccff
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-01-23 21:35:00 +0000

    latest update code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Using moement to enter today's date at the top of that page
+// Using moment to enter today's date at the top of that page
 var today = moment();
 let dayWeek = today.format("[Happy] dddd Do MMMM[!]")
 $("#currentDay").text(dayWeek);
 console.log(dayWeek);
 
 
-//On click  that sets items - current time and value that is entered into the text area
+//On click that sets items - current time and value that is entered into the text area
 $(document).ready(function () {
     $('.saveBtn').on('click', function () {
       let value = $(this).siblings('.work-description').val()
@@ -14,14 +14,14 @@ $(document).ready(function () {
       localStorage.setItem(time, JSON.stringify(value))
     })
 
-//update hours function
+// Update hours function - identifying where the currentBlockTime is before, the same, or after the currentHour (to ensure correct CSS formatting can take place)
     function updateHours() {
         let currentHour = moment().hour();
         console.log(currentHour)
         
         $('.time-area').each(function() {
             let currentBlockTime = parseInt($(this).attr('id').split('-')[0])
-            
+            console.log(currentBlockTime)
             if (currentBlockTime < currentHour) {
                 $(this).addClass('past');
             } else if (currentBlockTime === currentHour) {
