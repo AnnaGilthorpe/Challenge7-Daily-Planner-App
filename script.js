17601404d00240c4e91870a1ded0c970635787c5
Anna Gilthorpe <annagilthorpe@gmail.com>
2023-01-24 12:04:43 +0000

    time area statements debug

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ console.log(dayWeek);
 //On click that sets items - current time and value that is entered into the text area
 $(document).ready(function () {
     $('.saveBtn').on('click', function () {
-      let value = $(this).siblings('.work-description').val()
-      let time = $(this).parent().attr('id')
+        let value = $(this).siblings('.work-description').val()
+        let time = $(this).parent().attr('id')
 
-      localStorage.setItem(time, JSON.stringify(value))
+        localStorage.setItem(time, JSON.stringify(value))
     })
 
-// Update hours function - identifying where the currentBlockTime is before, the same, or after the currentHour (to ensure correct CSS formatting can take place)
+    // Update hours function - identifying where the currentBlockTime is before, the same, or after the currentHour (to ensure correct CSS formatting can take place)
     function updateHours() {
         let currentHour = moment().hour();
         console.log(currentHour)
-        
-        $('.time-area').each(function() {
+
+        $('.time-area').each(function () {
             let currentBlockTime = parseInt($(this).attr('id').split('-')[0])
             console.log(currentBlockTime)
             if (currentBlockTime < currentHour) {
@@ -27,23 +27,25 @@ $(document).ready(function () {
             } else if (currentBlockTime === currentHour) {
                 $(this).removeClass('past')
                 $(this).addClass('present');
-             } else (currentBlockTime > currentHour) {
-                 $(this).removeClass('past')
+            } else if (currentBlockTime > currentHour)  {
+                $(this).removeClass('past')
                 $(this).removeClass('present')
-                $(this).Class('future');
-             }}   
-
+                $(this).addClass('future');
+            }
+        }
 
         )
-//getting the page to update every 9mins
+    }
+
+    //getting the page to update every 9mins
     updateHours()
     setInterval(updateHours, 540000)
 
 
     // bug, work out why local storage isnt dispalying after refresh
 
-   
-   
+
+
     $('#9-am .work-description').val(localStorage.getItem('9-am'))
     $('#10-am .work-description').val(localStorage.getItem('10-am'))
-    }})
+})
